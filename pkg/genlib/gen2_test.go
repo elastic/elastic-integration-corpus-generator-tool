86a9df777c5a752886ebd3f6a8b8ad5f62f28dc3
Edoardo Tenani <edoardo.tenani@pm.me>
2022-12-07 09:12:38 +0100

    support object keys

--- pkg/genlib/gen2_test.go
+++ pkg/genlib/gen2_test.go
@@ -3,14 +3,14 @@ package genlib
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"os"
 	"testing"
 
+	"github.com/elastic/elastic-integration-corpus-generator-tool/pkg/genlib/config"
 	"github.com/elastic/elastic-integration-corpus-generator-tool/pkg/genlib/fields"
 )
 
-func Test_Gen2(t *testing.T) {
+func Test_Gen2Plain(t *testing.T) {
 	ctx := context.Background()
 	// flds, err := fields.LoadFields(ctx, fields.ProductionBaseURL, "endpoint", "process", "8.2.0")
 	flds, err := fields.LoadFields(ctx, fields.ProductionBaseURL, "aws", "vpcflow", "1.28.0")
@@ -20,6 +20,72 @@ func Test_Gen2(t *testing.T) {
 	}
 
 	dat, err := os.ReadFile("testdata/template.tpl")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	g, err := NewGen2(dat, Config{}, flds)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+
+	state := NewGenState()
+
+	err = g.Emit(state, &buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(buf.String())
+}
+
+func Test_Gen2PlainWithConfig(t *testing.T) {
+	ctx := context.Background()
+	// flds, err := fields.LoadFields(ctx, fields.ProductionBaseURL, "endpoint", "process", "8.2.0")
+	flds, err := fields.LoadFields(ctx, fields.ProductionBaseURL, "aws", "vpcflow", "1.28.0")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dat, err := os.ReadFile("testdata/template.tpl")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cfg, err := config.LoadConfig("testdata/aws-vpcflow.conf.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	g, err := NewGen2(dat, cfg, flds)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+
+	state := NewGenState()
+
+	err = g.Emit(state, &buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(buf.String())
+}
+
+func Test_Gen2WithObjects(t *testing.T) {
+	ctx := context.Background()
+	flds, err := fields.LoadFields(ctx, fields.ProductionBaseURL, "gcp", "gke", "2.15.1")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dat, err := os.ReadFile("testdata/gcp-2.15.1.tpl")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -39,7 +105,7 @@ func Test_Gen2(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	fmt.Println(buf.String())
+	t.Log(buf.String())
 }
 
 // go test -bench=. -v -benchmem -count=20 -run "Benchmark_Gen2"

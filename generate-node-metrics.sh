6991fc4d50efa475b37c4eec87899986f770efe4
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2023-10-06 15:19:41 +0300

    Use period type condig for  timestamp generation

--- generate-node-metrics.sh
+++ generate-node-metrics.sh
@@ -1,23 +1,3 @@
-function exit_handler()
-{
-    echo $1 $2 $3 $4
-    replace "hour" $1 "placeholder_hour"
-    replace "minute" $2 "placeholder_min"
-    replace "second" $3 "placeholder_sec"
-    replace "timedate" $4 "placeholder_date"
-    rm data.json
-    echo "exiting"
-    exit
-}
-
-function replace()
-{
-    tmpfile=$(mktemp)
-    cp ./assets/templates/kubernetes.node/schema-b/configs.yml "$tmpfile" 
-    awk '/name: '$1'/{ rl = NR + 1 } NR == rl { gsub( "'$2'","'"$3"'") } 1' "$tmpfile" > ./assets/templates/kubernetes.node/schema-b/configs.yml
-    rm "$tmpfile" 
-}
-
 Help()
 {
    # Display Help
@@ -57,48 +37,12 @@ if [ -z "${ELASTIC_HOST}" ]; then
 fi
 
 events=$1
-three=3
-timedate=`date +%Y-%m-%d`
-hour=`date +"%H"`
-hour=10#$hour
-utchour="$((hour-three))"
-if [ $utchour -lt 10 ]
-then
-    utchour=0$utchour
-fi
-minute=`date +"%M"` 
-second=`date +"%S"`.328Z
-oldutchour=$utchour
-oldminute=$minute
-oldsecond=$second
-
-replace "timedate" "placeholder_date" $timedate
-replace "hour" "placeholder_hour" $utchour
-replace "minute" "placeholder_min" $minute
-replace "second" "placeholder_sec" $second
-
-trap "exit_handler $oldutchour $oldminute $oldsecond $timedate" EXIT
 while true
 do
-    sleep 10
-    hour=`date +"%H"`
-    hour=10#$hour
-    utchour="$((hour-three))"
-    if [ $utchour -lt 10 ]
-    then
-        utchour=0$utchour
-    fi
-    minute=`date +"%M"` 
-    second=`date +"%S"`.328Z
-    replace "hour" $oldutchour $utchour
-    replace "minute" $oldminute $minute
-    replace "second" $oldsecond $second
-   
-    oldutchour=$utchour
-    oldminute=$minute
-    oldsecond=$second
-    trap "exit_handler $oldutchour $oldminute $oldsecond $timedate" EXIT
-    output=`./elastic-integration-corpus-generator-tool generate-with-template ./assets/templates/kubernetes.node/schema-b/gotext.tpl ./assets/templates/kubernetes.node/schema-b/fields.yml -c ./assets/templates/kubernetes.node/schema-b/configs.yml -y gotext -t $1`
+    datenow=`date -u +"%FT%T.%3Z"`
+    echo $datenow
+    output=`./elastic-integration-corpus-generator-tool generate-with-template ./assets/templates/kubernetes.node/schema-b/gotext.tpl ./assets/templates/kubernetes.node/schema-b/fields.yml -c ./assets/templates/kubernetes.node/schema-b/configs.yml -y gotext -t $1 -n $datenow`
+    
     #Define multi-character delimiter
     delimiter=" "
     #Concatenate the delimiter with the main string
@@ -114,7 +58,8 @@ do
     generatedfile="`echo ${myarray[2]} ${myarray[3]}`"
     echo ""
     echo $generatedfile
-    echo $utchour:$minute:$second
     mv "$generatedfile" data.json
     curl --location --request POST -u ${ELASTIC_USERNAME}:${ELASTIC_PASS} ${ELASTIC_HOST}/metrics-kubernetes.node-default/_bulk  --header 'Content-Type: application/x-ndjson' --data-binary @data.json
+    rm data.json
+    sleep 30
 done
